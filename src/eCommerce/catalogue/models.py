3eb547f636a804436d431e8b5f638e7344d0cdd8
Luka Abeywickrama <luka.abeywickrama@hotmail.co.uk>
2020-10-16 22:52:51 +0100

    image location added to image model

--- src/eCommerce/catalogue/models.py
+++ src/eCommerce/catalogue/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class Item(models.Model):
     name = models.CharField(max_length=250)
     price = models.IntegerField()
-    image_location = models.CharField(max_length=250)
+    image_location = models.CharField(max_length=250, blank=True, default="")
     discount_price = models.IntegerField(blank=True, null=True)
     slug = models.SlugField()
 
