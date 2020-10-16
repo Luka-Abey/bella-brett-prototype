e89c272208aef871783adeeeba1a41f764723c04
Luka Abeywickrama <luka.abeywickrama@hotmail.co.uk>
2020-10-16 22:26:58 +0100

    models

--- src/eCommerce/eCommerce/models.py
+++ src/eCommerce/eCommerce/models.py
@@ -3,15 +3,22 @@ from django.contrib.auth.models import User
 
 
 class Item(models.Model):
-    pass
+    name = models.CharField()
 
 
 class OrderItem(models.Model):
-    pass
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    ordered = models.BooleanField(default=False)
+    quantity = models.IntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.quantity} of {self.item.name}"
 
 
 class Order(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    items = models.ManyToManyField(OrderItem)
     ordered = models.BooleanField(auto_now_add=False)
     start_date = models.DateTimeField(auto_now_add=True)
     ordered_date = models.DateTimeField()
