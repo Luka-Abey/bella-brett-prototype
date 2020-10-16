20414f632572eb7f44513af097f4f2c2b5cdbe6f
Luka Abeywickrama <luka.abeywickrama@hotmail.co.uk>
2020-10-16 22:38:26 +0100

    item model

--- src/eCommerce/catalogue/admin.py
+++ src/eCommerce/catalogue/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Item, Order, OrderItem
 
-# Register your models here.
+class ItemAdmin(admin.ModelAdmin):
+    list_display = [
+        'name',
+        'price',
+        'discount_price'
+    ]
+
+
+admin.site.register(Item)
+admin.site.register(Order)
+admin.site.register(OrderItem)
