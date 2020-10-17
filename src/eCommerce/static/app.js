53313dcac0618a2386ec80eb91509c0c6fedcc50
Luka Abeywickrama <luka.abeywickrama@hotmail.co.uk>
2020-10-17 13:29:40 +0100

    working on navbar design

---
+++ src/eCommerce/static/app.js
@@ -0,0 +1,27 @@
+const navSlide = () => {
+  const burger = document.querySelector(`.burger`);
+  const nav = document.querySelector(`.nav-links`);
+  const navLinks = document.querySelectorAll(`.nav-links li`);
+
+  burger.addEventListener('click', () => {
+    nav.classList.toggle('nav-active');
+
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = '';
+      }
+      else {
+        link.style.animation = `navLinkFade 0.4s ease forwards ${index / 7}s`;
+
+      }      
+    })
+  })
+
+
+}
+
+navSlide();
+
+// const app = () => {
+//   navSlide();
+// }
