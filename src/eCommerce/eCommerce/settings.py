25f301ae1c220eaf70573417af0e97d9cd862430
Luka Abeywickrama <luka.abeywickrama@hotmail.co.uk>
2020-10-17 11:45:16 +0100

    css working

--- src/eCommerce/eCommerce/settings.py
+++ src/eCommerce/eCommerce/settings.py
@@ -106,5 +106,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+    '/src/eCommerce/static/',
+]
 STATIC_URL = '/static/'
