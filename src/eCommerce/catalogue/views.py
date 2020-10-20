8ddc9cb5abbf9c44b3badfcb6855de71286ddbf8
Luka Abeywickrama <luka.abeywickrama@hotmail.co.uk>
2020-10-20 13:54:56 +0100

    basket time

--- src/eCommerce/catalogue/views.py
+++ src/eCommerce/catalogue/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'index.html')
+
+
+def basket(request):
+    return render(request, 'basket.html')
+
+
+def checkout(request):
+    return render(request, 'checkout.html')
--- src/eCommerce/eCommerce/urls.py
+++ src/eCommerce/eCommerce/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('catalogue.urls'))
+    path('', include('catalogue.urls')),
+    path('basket/', include('catalogue.urls'))
 ]
