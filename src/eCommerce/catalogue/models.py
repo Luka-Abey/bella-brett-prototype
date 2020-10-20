39f20cb01183f80cc16e0285ebae7afa99c0da89
Luka Abeywickrama <luka.abeywickrama@hotmail.co.uk>
2020-10-20 19:16:23 +0100

    almost dynamic

--- src/eCommerce/catalogue/models.py
+++ src/eCommerce/catalogue/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class Item(models.Model):
     name = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    image = models.ImageField(null=True, blank=True, upload_to="media/images/")
+    image = models.ImageField(null=True, blank=True, upload_to="images/")
     description = models.CharField(max_length=255)
 
     def __str__(self):
@@ -25,9 +25,11 @@ class OrderItem(models.Model):
 class Order(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     items = models.ManyToManyField(OrderItem)
+    name = models.CharField(max_length=255)
+    delivery_address = models.CharField(max_length=255)
     ordered = models.BooleanField(default=False)
     start_date = models.DateTimeField(auto_now_add=True)
-    ordered_date = models.DateTimeField()
+    date = models.DateTimeField()
 
     def __str__(self):
         return self.user.username
