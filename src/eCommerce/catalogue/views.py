e9cd6ee47ad3b8a1fc26330bc3ea46db1d6e5fc6
Luka Abeywickrama <luka.abeywickrama@hotmail.co.uk>
2020-10-21 20:41:59 +0100

    setting up class based views

--- src/eCommerce/catalogue/views.py
+++ src/eCommerce/catalogue/views.py
@@ -21,5 +21,16 @@ class HomeView(ListView):
 
 def add_to_cart(request, id):
     item = get_object_or_404(Item, id=id)
-    order_item = OrderItem.objects
+    order_item = OrderItem.objects.create(item=item)
+    order_qs = Order.objects.filter(user=request.user, ordered=False)
+    if order_qa.exists():
+        order = order_qs[0]
+        if order.items.filter(item__id=item.id).exists():
+            order_item.quantity +=1 ;
+            order_item.save()
+    
+    else:
+        order = Order.objects.create(user=request.user)
+        order.items.add(order_item)
+    
 
