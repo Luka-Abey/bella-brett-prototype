c41da3516f8c979bc3760703c2ed63e5416dbe25
Luka Abeywickrama <luka.abeywickrama@hotmail.co.uk>
2020-10-23 11:54:53 +0100

    add to cart logic begins

--- src/eCommerce/static/js/basket.js
+++ src/eCommerce/static/js/basket.js
@@ -1,1 +1,9 @@
-console.log("hello")
+var updateBtns = document.getElementsByClassName('update-cart')
+
+for(var i = 0; i < updateBtns.length; i++){
+  updateBtns[i].addEventListener('click', function(){
+    var itemId = this.dataset.item
+    var action = this.dataset.action
+    console.log(itemId, action)
+  })
+}
