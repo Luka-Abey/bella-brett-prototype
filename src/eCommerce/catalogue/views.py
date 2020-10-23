91d2eaf981fed52708fff13919faaa298a5769ec
Luka Abeywickrama <luka.abeywickrama@hotmail.co.uk>
2020-10-23 11:42:52 +0100

    add to cart logic begins

--- src/eCommerce/catalogue/views.py
+++ src/eCommerce/catalogue/views.py
@@ -3,7 +3,19 @@ from django.views.generic import ListView, DetailView
 from .models import Item, Order, OrderItem
 
 def checkout(request):
-    return render(request, 'checkout.html')
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, ordered=False) 
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = {
+            'get_cart_total': 0,
+            'get_cart_items': 0
+        }
+
+    context = {'items': items, 'order': order}   
+    return render(request, 'checkout.html', context)
 
 
 def basket(request):
@@ -20,7 +32,6 @@ def basket(request):
 
     context = {'items': items, 'order': order}   
     return render(request, 'basket.html', context)
-   
 
 
 class HomeView(ListView):
